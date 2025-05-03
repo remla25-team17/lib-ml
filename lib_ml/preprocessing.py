1cb02acd88d1fca3a441aeb7410ffe7d679584a3
Marina Escribano Esteban <M.EscribanoEsteban@student.tudelft.nl>
2025-05-03 17:04:45 +0200

    Adding preprocessing logic

--- lib_ml/preprocessing.py
+++ lib_ml/preprocessing.py
@@ -1,6 +1,35 @@
 import re
+import nltk
 
-def preprocessing(text):
-    text = text.lower()
-    text = re.sub(r"[^a-z0-9\s]", "", text)
-    return text
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('stopwords')
+
+# Define the preprocessing function
+def preprocess_reviews(dataset):
+    """
+    Preprocesses text data from a dataset.
+
+    Args:
+        dataset (pd.DataFrame): The dataset containing reviews.
+
+    Returns:
+        corpus: A list of preprocessed reviews.
+    """
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    if 'not' in all_stopwords:
+        all_stopwords.remove('not')
+
+    corpus = []
+    max_index = len(dataset) 
+
+    for i in range(0, max_index):
+        review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+        review = review.lower().split()
+        review = [ps.stem(word) for word in review if word not in set(all_stopwords)]
+        review = ' '.join(review)
+        corpus.append(review)
+
+    return corpus
